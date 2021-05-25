b083b35298298e52c56b4f7cd55aeebc8e0fe33e
Kirk <kirkbalop@gmail.com>
2021-05-25 13:22:01 -0700

    added README//adjusted nomenclature

---
+++ README.md
@@ -0,0 +1,28 @@
+# The Bank of React
+
+## Description
+
+The Bank of React is a personal finance tool built to assist Users in tracking and budgeting expenses. The User's input is plotted on both a table and a line chart for easy interpretation.
+
+## Tech Used
+
+- MERN Stack
+- JavaScript
+- GraphQL
+- CSS
+- JSON Web Token Authentication
+- IndexDB
+
+## Usage
+
+Users must create an account and log in for data to be entered and persisted. Users enter data in the form fields and choose expense or deposit, and the data is transferred to the table/graph.
+
+## Deployment 
+
+https://intense-brushlands-21772.herokuapp.com/
+
+## Contact
+
+Feel free to contact the creator with any further inquiries:
+- Email: KirkBalop@gmail.com
+- Github: https://github.com/kirkbalop
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -12,8 +12,8 @@ const Home = () => {
         <div className="form">
           <input type="text" id="t-name" placeholder="Name of transaction" />
           <input type="number" min="0" id="t-amount" placeholder="Transaction amount" />
-          <button id="add-btn" onClick={() => sendTransaction(true)}><i className="fa fa-plus buttons" ></i> Add Funds</button>
-          <button id="sub-btn" onClick={() => sendTransaction(false)}><i className="fa fa-minus"></i> Subtract Funds</button>
+          <button id="add-btn" onClick={() => sendTransaction(true)}><i className="fa fa-plus buttons" ></i>Deposit</button>
+          <button id="sub-btn" onClick={() => sendTransaction(false)}><i className="fa fa-minus"></i>Expense</button>
           <p className="error"></p>
         </div>
     
